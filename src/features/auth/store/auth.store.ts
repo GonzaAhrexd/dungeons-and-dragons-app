2dbef66580f0eb7544e1e2a2863a58fe8cc85d64
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2026-07-30 22:14:35 -0300

    chore(): Delete .env

--- src/features/auth/store/auth.store.ts
+++ src/features/auth/store/auth.store.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 import type { UserProfile } from '../interfaces'
+
 interface AuthState {
   token: string | null
   user: UserProfile | null
