6e32335089f1761d21674475e719ef9f50b75819
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2026-09-02 19:40:55 -0300

    style(Dropdown, CampaignInvitations): Improve invitations dropdown

--- src/features/campaigns/components/Campaigns/components/CampaignInvitations/CampaignInvitations.langs.ts
+++ src/features/campaigns/components/Campaigns/components/CampaignInvitations/CampaignInvitations.langs.ts
@@ -3,23 +3,27 @@ import type { LanguagesText } from '@/features/langs/interfaces'
 interface CampaignInvitationsText {
   title: string
   description: string
+  invitatedBy: string
   acceptInvitation: string
   declineInvitation: string
 }
 
-export const campaignsInvitationsText: LanguagesText<CampaignInvitationsText> = {
-  en: {
-    title: 'Campaign Invitations',
-    description:
-      'You have been invited to join the following campaigns. Accept or decline the invitations below.',
-    acceptInvitation: 'Accept Invitation',
-    declineInvitation: 'Decline Invitation',
-  },
-  es: {
-    title: 'Invitaciones a Campañas',
-    description:
-      'Has sido invitado a unirte a las siguientes campañas. Acepta o rechaza las invitaciones a continuación.',
-    acceptInvitation: 'Aceptar Invitación',
-    declineInvitation: 'Rechazar Invitación',
-  },
-}
+export const campaignsInvitationsText: LanguagesText<CampaignInvitationsText> =
+  {
+    en: {
+      title: 'Invitations',
+      description:
+        'Accept or decline the invitations below.',
+      invitatedBy: 'Invited by',
+      acceptInvitation: 'Accept Invitation',
+      declineInvitation: 'Decline Invitation',
+    },
+    es: {
+      title: 'Invitaciones',
+      invitatedBy: 'Invitado por',
+      description:
+        'Acepta o rechaza las invitaciones a continuación.',
+      acceptInvitation: 'Aceptar Invitación',
+      declineInvitation: 'Rechazar Invitación',
+    },
+  }
--- src/features/campaigns/components/Campaigns/components/CampaignInvitations/CampaignInvitations.tsx
+++ src/features/campaigns/components/Campaigns/components/CampaignInvitations/CampaignInvitations.tsx
@@ -6,6 +6,7 @@ import {
 import { useText } from '@/features/langs/hooks/useText'
 import { campaignsInvitationsText } from './CampaignInvitations.langs'
 import { Button } from '@/shared/ui/Button/Button'
+import { Icon } from '@/shared/ui/Icon/Icon'
 export const CampaignInvitations = () => {
   const text = useText(campaignsInvitationsText)
 
@@ -34,7 +35,10 @@ export const CampaignInvitations = () => {
 
   return (
     <div className="cmp-campaign-invitations">
-      <h1>{text.title()}</h1>
+      <div className="header">
+        <Icon icon="fa-solid fa-scroll" />
+        <h1>{text.title()}</h1>
+      </div>
       <p>{text.description()}</p>
 
       <div className="invitations-list">
@@ -42,17 +46,22 @@ export const CampaignInvitations = () => {
           <div key={invitation.id} className="invitation">
             <div className="invitation-header">
               <h2>{invitation.campaignName}</h2>
-              <p>Invited by: {invitation.gamemasterUsername}</p>
+              <p>
+                {text.invitatedBy()}:
+                <span> {invitation.gamemasterUsername}</span>
+              </p>
             </div>
             <div className="invitation-actions">
               <Button
                 icon="fa-solid fa-check"
                 handlingClass="accept"
+                loader
                 onClick={() => handleAcceptInvitation(invitation.id)}
               />
               <Button
                 icon="fa-solid fa-times"
                 handlingClass="decline"
+                loader
                 onClick={handleDeclineInvitation(invitation.id)}
               />
             </div>
